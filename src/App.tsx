6abb9528392e224d323fdf72ae535e52823ef655
sdalmacen <almacenstephen@gmail.com>
2024-09-21 14:42:57 +0800

    jwt and session- No Logout yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // src/App.tsx
 // src/App.tsx
 import './App.css'; // Make sure this line is present
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Common/Header/Header';
 import Footer from './components/Common/Footer/Footer';
+import Cookies from 'js-cookie';
 
 const App: React.FC = () => {
   const navigate = useNavigate();
@@ -16,9 +17,17 @@ const App: React.FC = () => {
     navigate('/dashboard');
   };
 
+  useEffect(() => {
+    const web_jwt = Cookies.get('web_jwt'); // Get token from cookie\
+    if (web_jwt) {
+      navigate('/dashboard'); // Redirect to dashboard if token is present
+    }
+  }, [navigate]);
+  
   return (
     <div className="App">
       <Header />
+      <br></br>
       <main className="main-content"> {/* Added main tag */}
         <Routes>
           <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />
--- src/components/Login/Login.service.ts
+++ src/components/Login/Login.service.ts
@@ -1,5 +1,3 @@
-// src/components/Login/Login.service.ts
-
 import axios from 'axios';
 import { LoginRequest, LoginResponse } from './Login.types';
 
@@ -9,22 +7,25 @@ if (!apiUrl) {
   throw new Error('REACT_APP_API_URL is not defined');
 }
 
+// Login service function that sends login request to the server
 export const LoginService = async (loginRequest: LoginRequest): Promise<LoginResponse> => {
   try {
-    const response = await axios.post(`${apiUrl}/api/login`, loginRequest);
-
+    const response = await axios.post(
+      `${apiUrl}/api/login`, 
+      loginRequest, 
+      { withCredentials: true } // Enable cookies to be sent with the request
+    );
+    console.log(response);
     if (response.data) {
-      // Store the token securely (e.g., in local storage)
-      localStorage.setItem('authToken', "I am ze token of death");
-      return { success: true, token: "I am ze token of death" };
+      return { success: true, token: response.data.token };
     }
 
-    return { success: false, errorMessage: 'Login failed: no token returned' };
+    return { success: false, errorMessage: 'Login failed' };
   } catch (error) {
     const errorMessage = axios.isAxiosError(error) && error.response 
       ? error.response.data.message || 'An error occurred during login'
       : 'An unexpected error occurred';
-      
+
     console.error('An error occurred:', errorMessage);
     return { success: false, errorMessage };
   }
